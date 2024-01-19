1d2f984a64e61736acc3e1c253cfa873ad72cafc
lucastimho <lucas.t.ho.lh@gmail.com>
2024-01-19 09:21:16 -0600

    adds questions for lab1

---
+++ lab1/lab1_lucas_ho.R
@@ -0,0 +1,59 @@
+#Task 1
+getwd()
+
+#Task 2
+ddt <- read.csv("DDT.csv")
+
+#First six lines
+head(ddt)
+
+#Task 3
+#Variable in DDT
+names(ddt)
+
+#How many qualitative variables are in DDT?
+2
+
+#How many quantitative variables are in DDT?
+4
+
+#How many species are in the ddt data set?
+
+#Subset the ddt data set so that we see only the observations with SPECIES,  LMBASS and WEIGHT bigger than 800gms
+
+#Subset the ddt data set so that we see only those observations where the RIVER is SCM   and DDT bigger than 4.0
+
+#Task 4
+#Find the mean length of fish in the ddt data frame
+
+#What is the standard deviation of the weight of fish in the ddt data frame?
+
+#Is this the plot of LENGTH vs WEIGHT from the ddt data frame?
+
+#If v=1:20 what is the last value of v/20?
+
+#Task 5
+#Make a table of the rivers.
+
+#Make a color coded barplot of the rivers
+
+#Make a table of the rivers crossed with the species of fish
+
+#Make a barplot of the rivers crossed with the species of fish.
+
+#Task 6
+#Make a pie chart of Fish SPECIES
+
+#Make a pie chart of RIVERS
+
+#Task 7
+#Make a boxplot of the variable DDT
+
+#Make a boxplot of the variable WEIGHT
+
+#Make a boxplot of the variable LENGTH
+
+#Task 8
+#Make a coplot of LENGTH Vs WEIGHT given RIVER
+
+#Make a coplot of DDT Vs WEIGHT given SPECIES
