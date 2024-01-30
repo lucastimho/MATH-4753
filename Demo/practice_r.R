db658d45d6101c8adfbe84efb3e007e3b595ccc8
lucastimho <lucas.t.ho.lh@gmail.com>
2024-01-29 19:11:28 -0600

    completes lab2

--- Demo/practice_r.R
+++ Demo/practice_r.R
@@ -1,14 +1,24 @@
+library(dplyr)
 ddt <- read.csv("DDT.csv")
 
 summary(ddt)
 
 head(ddt)
 
-with(ddt, ddt[LENGTH > 49 & WEIGHT < 1200,])
+# with(ddt, ddt[LENGTH > 49 & WEIGHT < 1200,])
+# 
+# ddt[which.max(ddt$LENGTH), "DDT"]
+# 
+# L <- ddt$LENGTH
+# z <- (L-mean(L))/sd(L)
+# z
+# L[abs(z)>3]
 
-ddt[which.max(ddt$LENGTH), "DDT"]
+df1 <- ddt %>% filter(LENGTH > 30 & DDT < 1000)
+df1
 
-L <- ddt$LENGTH
-z <- (L-mean(L))/sd(L)
-z
-L[abs(z)>3]
+ddt %>% select(c(SPECIES, RIVER)) %>% table() %>% addmargins()
+
+ddt %>% summarise(mean = mean(LENGTH))
+
+ddt %>% group_by(SPECIES) %>% summarise (mean = mean(WEIGHT), n = n())
