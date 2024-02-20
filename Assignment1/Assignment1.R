3d933ab9abf9363c5c9b466398ffc42accb7a439
lucastimho <lucas.t.ho.lh@gmail.com>
2024-02-19 20:02:39 -0600

    adds Q4 and Q5

--- Assignment1/Assignment1.R
+++ Assignment1/Assignment1.R
@@ -24,3 +24,32 @@ coplot(WEIGHT~LENGTH | RIVER*SPECIES, data=ddt, col="Blue", pch=1.2, xlab="Lengt
 ddt[ddt$RIVER=="FCM" & ddt$SPECIES=="CCATFISH",]
 
 # Q3
+# Quantitative
+# Quantitative
+# Qualitative
+# Quantitative
+# Qualitative
+# Quantitative
+# Qualitative
+
+# Q4
+# Simple, stratified, cluster, and systematic random sampling
+# Simple random sampling ensures that every subset of fixed size in the population has the same chance of being included in the sample
+# Stratified random sampling is typical used when the experimental units associated with the population can be separated into two or more groups of units where the characteristics of the units are more similar within the units than across the units
+# Cluster random sampling is used to sample natural groupings of experimental units first, then collect data from all experimental units within each cluster.
+# Systematic random sampling involves systematically selecting every kth experimental unit from list of all experimental units
+
+# Q5
+mtbe <- read.csv("MTBE.csv")
+
+ind=sample(1:223,5,replace=FALSE)
+
+mtbe[ind,]
+
+mtbeo = na.omit(mtbe)
+
+depth = mtbeo[mtbeo$Aquifier=="Bedrock",]$Depth
+
+sd(depth)
+
+# Q6
